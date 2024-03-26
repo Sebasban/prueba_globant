5e8a716b752dfaba66d0aca3f471e086aeef33b0
Sebastian <sebastian@MacBook-Pro-de-Sebastian.local>
2024-03-26 01:49:56 -0500

    se crea el archivo docker

--- test.py
+++
@@ -1,8 +0,0 @@
-import pandas as pd 
-
-df = pd.read_csv(r'resources/departments.csv', sep = ';')
-#print(df.iloc[1])
-#print(df.head())
-for _, row in df.iterrows():
-    values = list(row)
-    print(tuple(row))
